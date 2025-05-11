26b678cf6ceb2e6e4e428a55013f8e853fc5a059
Timo255 <104308259+Timo255@users.noreply.github.com>
2025-05-11 19:23:39 +0300

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
     'https://681e1eb2f162b6ba68ac4d28--soft-cactus-1fe087.netlify.app/',
-    'https://e-commerce2-0-1.onrender.com/'
+    'https://e-commerce2-0-1.onrender.com'
 ]
 
 module.exports = allowedOrigins;
