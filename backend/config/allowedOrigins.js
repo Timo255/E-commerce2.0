cc90612783bb2faee355dc79639dc42be3b55307
Timo255 <104308259+Timo255@users.noreply.github.com>
2025-05-14 10:39:51 +0300

    origin url

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'https://furnitur3store.netlify.app',
+    'https://e-commerce2-0-xh7h.onrender.com'
 ]
 
 module.exports = allowedOrigins;
