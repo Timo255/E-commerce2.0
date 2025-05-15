3b30b1f02a4f401a0559ced181befd543709b1a3
Timo255 <104308259+Timo255@users.noreply.github.com>
2025-05-15 16:29:34 +0300

    url origin update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
     'https://furnitur3store.netlify.app',
-    'https://681e1eb2f162b6ba68ac4d28--soft-cactus-1fe087.netlify.app',
+    'https://e-commerce2-0-1.onrender.com',
     'https://e-commerce2-0-xh7h.onrender.com'
 ]
 
